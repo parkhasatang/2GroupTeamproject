39189835a1f3b49feea1195242a7ba72fe6a640a
Kim-Hj0 <149381535+Kim-Hj0@users.noreply.github.com>
2023-12-15 18:10:55 +0900

    Tower 스크립트 추가 작성 및 등등.
    ObjectDetector 스크립트 작성.
    Tile 스크립트 작성.
    타워 생성.
    타워가 이미 자리에 생성되어 있다면, 그 자리 생성 x

--- Assets/Scripts/TowerSpawner.cs
+++ Assets/Scripts/TowerSpawner.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class TowerController : MonoBehaviour
+public class TowerSpawner : MonoBehaviour
 {
 
     [SerializeField]
@@ -11,6 +12,18 @@ public class TowerController : MonoBehaviour
 
     public void SpawnTower(Transform tileTransform)
     {
+        Tile tile = tileTransform.GetComponent<Tile>();
+        
+        //타워 생성 가능 여부 확인.
+        //같은 위치에 타워가 여러 개면 안되니까, 타워가 있다면 건설 x
+        if(tile.IsBuildTower == true)
+        {
+            return;
+        }
+
+        //타워가 건설되어 있음으로 설정
+        tile.IsBuildTower = true;
+
         //선택한 타일의 위치에 타워 건설.
         Instantiate(towerPrefab, tileTransform.position, Quaternion.identity);
     }
