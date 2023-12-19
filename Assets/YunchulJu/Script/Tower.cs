60825c29c90edb9117d2080146c6f9d89a7ea299
scotcy <kimjh470@naver.com>
2023-12-19 11:25:26 +0900

    feat : 타워 공격 구현 MainScene에 병합
    todo : 멀티샷 오류가 있어서 단일 공격 구현 필요

--- Assets/YunchulJu/Script/Tower.cs
+++ Assets/YunchulJu/Script/Tower.cs
@@ -7,10 +7,7 @@ public class Tower : MonoBehaviour
     // 공격력, 사정거리, 공격쿨타임, (공격유형, 공격속성) 에너미서치, 설치전 공격범위 보이게
     public int AttackDamage = 10; //공격력
     public float AttackRange = 10f; //공격범위
-    public float AttackCooltime = 10f; //매 공격간의 쿨타임(현재 10초)
-    public LayerMask EnemyLayer; //공격범위내 몬스터 탐지
-    public Transform AttackPoint; //공격지점
-    public float LastAttackTime; //이전에 공격이 발동한 시간
+    public float AttackCooltime = 1f; //매 공격간의 쿨타임(현재 10초)
     private float CurrentCooldown = 0f; // 실시간으로 줄어드는 공격 쿨타임
     public GameObject bulletPrefab;
     
