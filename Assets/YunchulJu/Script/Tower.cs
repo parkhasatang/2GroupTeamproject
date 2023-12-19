0878d23b41d5cd2d858044f95f41c81422afd7c6
wind12wind <dbscjfwn@gmail.com>
2023-12-19 12:50:54 +0900

    feat.Tower_attack_nomal(One target)

--- Assets/YunchulJu/Script/Tower.cs
+++ Assets/YunchulJu/Script/Tower.cs
@@ -10,10 +10,15 @@ public class Tower : MonoBehaviour
     public float AttackCooltime = 1f; //매 공격간의 쿨타임(현재 10초)
     private float CurrentCooldown = 0f; // 실시간으로 줄어드는 공격 쿨타임
     public GameObject bulletPrefab;
-    
+    Collider2D closestEnemy = null;
+    float closestDistance = float.MaxValue;
+
 
     private void Update()
     {
+        closestEnemy = null;
+        closestDistance = float.MaxValue;
+
         CurrentCooldown -= Time.deltaTime; //실시간으로 쿨타임 감소.
         if (CurrentCooldown <= 0) //쿨타임이 0이되면 공격실행
         {
@@ -30,15 +35,32 @@ public class Tower : MonoBehaviour
         {
             if (collider.CompareTag("Enemy"))
             {
-                // 타워에서 총알 생성
-                Vector3 spawnPosition = transform.position;
-                Bullet bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity).GetComponent<Bullet>();
-                bullet.SetTarget(collider.transform, AttackDamage);
-                Debug.Log("타워의 공격");
+                float distanceToEnemy = Vector2.Distance(transform.position, collider.transform.position);
+
+                // 현재 적이 가장 가까운 적인지 확인
+                if (distanceToEnemy < closestDistance)
+                {
+                    closestEnemy = collider;
+                    closestDistance = distanceToEnemy;
+                }
+                //// 타워에서 총알 생성
+                //Vector3 spawnPosition = transform.position;
+                //Bullet bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity).GetComponent<Bullet>();
+                //bullet.SetTarget(collider.transform, AttackDamage);
+                //Debug.Log("타워의 공격");
             }
        
         }
+        if (closestEnemy != null)
+        {
+            // 타워에서 총알 생성
+            Vector3 spawnPosition = transform.position;
+            Bullet bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity).GetComponent<Bullet>();
+            bullet.SetTarget(closestEnemy.transform, AttackDamage);
+            Debug.Log("타워의 공격");
+        }
     }
+
 
     ////Collider2D[] Enemies = 
     ////foreach(Collider2D Enemy in enemies)
