6cf5b4fb0dbada124db545f544752d5e3f44b0fb
scotcy <kimjh470@naver.com>
2023-12-19 20:13:28 +0900

    del : SPUM폴더 삭제

--- Assets/SPUM/Script/SPUM_TexutreList.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SPUM_TexutreList : MonoBehaviour
-{
-    public List<Sprite> _spriteList = new List<Sprite>();
-}
