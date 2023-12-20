88c8a7aa574a4a6861b723ce37c8755da6136da2
kim-soo-ah <149459760+kim-soo-ah@users.noreply.github.com>
2023-12-20 16:11:40 +0900

    feat:"endPannelUI추가"

--- Assets/Sooah/PlayerHP.cs
+++ Assets/Sooah/PlayerHP.cs
@@ -18,6 +18,11 @@ public class PlayerHP : MonoBehaviour
         currentHP = maxHP; // 현재 체력을 최대 체력과 같게 설정
     }
 
+    private void Start()
+    {
+        UIManager.instance.stageClearAction += ResetPlayerHp;
+    }
+
     public void TakeDamage(float damage)
     {
         currentHP -= damage; // 현재 체력에서 데미지 만큼 감소
@@ -27,7 +32,7 @@ public class PlayerHP : MonoBehaviour
 
         if(currentHP <= 0) // 체력이 0되면 게임오버
         {
-
+            UIManager.instance.GameOver();
         }
     }
 
@@ -44,5 +49,11 @@ public class PlayerHP : MonoBehaviour
 
             yield return null;
         }
+    }
+
+    public void ResetPlayerHp()
+    {
+        maxHP = 20;
+        currentHP = maxHP;
     }
 }
--- Assets/Sooah/UIManager.cs
+++ Assets/Sooah/UIManager.cs
@@ -1,16 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
 public class UIManager : MonoBehaviour
 {
     public PlayerHP playerHP;
     public PlayerGold playerGold;
+    public GameObject endPannel;
     public static UIManager instance;
+
+    public event Action stageClearAction;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        StageClear();
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        endPannel.SetActive(true);
+    }
+
+    public void StageClear()
+    {
+        Time.timeScale = 1;
+        endPannel.SetActive(false);
+        stageClearAction?.Invoke();
+    }
+
+    public void IntroScene()
+    {
+        SceneManager.LoadScene("StartDeokScene");
     }
 }
